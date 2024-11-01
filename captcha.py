58e418b336d83f8177faf1d1e60846edf52225a7
James Allen <34049950+tjamesallen15@users.noreply.github.com>
2024-11-01 14:09:52 +0800

    Captcha Test

--- captcha.py
+++ captcha.py
@@ -17,31 +17,31 @@ pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesse
 
 
 
-sImagesPath = 'img/captcha3.png'
-mylist = []
+# sImagesPath = 'captcha/captcha3.jpg'
+# mylist = []
 
-def replace_chars(text):
-    list_of_numbers = re.findall(r'\d+', text)
-    result_number = ''.join(list_of_numbers)
-    return result_number
+# def replace_chars(text):
+#     list_of_numbers = re.findall(r'\d+', text)
+#     result_number = ''.join(list_of_numbers)
+#     return result_number
 
-for root, dirs, file_names in os.walk(sImagesPath):
-    for file_name in file_names:
-        img = cv2.imread(sImagesPath + file_name)
-        gry = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        thr = cv2.adaptiveThreshold(gry, 181, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 13, 10)
-        txt = pytesseract.image_to_string(thr, lang='eng',config='--psm 10 --oem 3 -c tessedit_char_whitelist=0123456789')
-        mylist.append(replace_chars(txt))
-        print(replace_chars(txt))
+# for root, dirs, file_names in os.walk(sImagesPath):
+#     for file_name in file_names:
+#         img = cv2.imread(sImagesPath + file_name)
+#         gry = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#         thr = cv2.adaptiveThreshold(gry, 181, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 13, 10)
+#         txt = pytesseract.image_to_string(thr, lang='eng',config='--psm 10 --oem 3 -c tessedit_char_whitelist=0123456789')
+#         mylist.append(replace_chars(txt))
+#         print(replace_chars(txt))
 
-with open('Output.txt', 'w') as f:
-    for i in mylist:
-        s = ''.join(map(str, i))
-        f.write(s + '\n')
+# with open('Output.txt', 'w') as f:
+#     for i in mylist:
+#         s = ''.join(map(str, i))
+#         f.write(s + '\n')
 
 
 
-# img = cv2.imread("img/captcha1.png")
+# img = cv2.imread("captcha/captcha3.jpg")
 # gry = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # (h, w) = gry.shape[:2]
 # gry = cv2.resize(gry, (w*2, h*2))
